237ad32f59563b508d71e6b63ba78a6f1ba35549
yessica60 <84580770+yessica60@users.noreply.github.com>
2023-11-28 21:14:34 -0400

    feat: create constant error

---
+++ src/constant/errors.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+    'Firebase: Error (auth/email-already-in-use).': 'Invalid user information, please change it.',
+    'Firebase: Error (auth/invalid-email).': 'Email is not valid.',
+    'Firebase: Error (auth/invalid-login-credentials).': 'Invalid login credentials.'
+  };
